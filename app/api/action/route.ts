cd4939652bc737cc5a7a2d199537a0a58000e1f8
lim0n3 <simonestaffa96@gmail.com>
2024-07-13 22:11:29 +0200

    add landing page

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -7,12 +7,17 @@ export const GET = async (req: NextRequest) => {
 
   const evmActionMetadata: EVMAction = {
     title: "Safe EVM Action",
-    description: "This is a sample EVM Action Safe Proposal that you can sign through a shortcut",
-    image: "https://placehold.co/955x500",
+    description:
+      "This is a sample EVM Action Safe Proposal that you can sign through a shortcut",
+    image: `${appURL()}/landing.png`,
     links: [
       {
-        targetUrl: `${appURL()}/api/signature?txHash=${searchParams.get('txHash')}&chain=${searchParams.get('chain')}`,
-        postUrl: `${appURL()}/api/signature/success?chain=${searchParams.get('chain')}`, // this will be a POST HTTP call
+        targetUrl: `${appURL()}/api/signature?txHash=${searchParams.get(
+          "txHash"
+        )}&chain=${searchParams.get("chain")}`,
+        postUrl: `${appURL()}/api/signature/success?chain=${searchParams.get(
+          "chain"
+        )}`, // this will be a POST HTTP call
         label: "Signature",
         type: ActionLinkType.SIGNATURE,
       },
