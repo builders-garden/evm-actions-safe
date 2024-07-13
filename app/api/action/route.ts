9e459701af409bf6740e4b4ca277b243b5b2e34c
PaoloRollo <paolo.rollo1997@gmail.com>
2024-07-13 20:27:52 +0200

    feat: update.

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -3,13 +3,15 @@ import { appURL } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+
   const evmActionMetadata: EVMAction = {
     title: "Safe EVM Action",
     description: "This is a sample EVM Action Safe Proposal that you can sign through a shortcut",
     image: "https://placehold.co/955x500",
     links: [
       {
-        targetUrl: `${appURL()}/api/signature`,
+        targetUrl: `${appURL()}/api/signature?txHash=${searchParams.get('txHash')}`,
         postUrl: `${appURL()}/api/signature/success`, // this will be a POST HTTP call
         label: "Signature",
         type: ActionLinkType.SIGNATURE,
